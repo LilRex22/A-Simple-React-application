dc6051d5d971df358a08197c992f4fa2a46c0f35
dunu rex <dunuchukwuemeka22@gmail.com>
2026-04-20 23:48:33 +0100

    refurbished the navbar and the navlink logics to include active links

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,16 +1,35 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { NavLink } from 'react-router-dom';
 
 function NavigationBar(props) {
+    const navlinks = [
+        {Name: 'Home', Path: '/'},
+        {Name: 'Employees', Path: '/employees'},
+        {Name: 'Pricing', Path: '/pricing'}
+    ]
+    
     return (
         <>
             <Navbar bg="dark" data-bs-theme="dark">
                 <Container fluid>
                     <Navbar.Brand href="/">Mployee</Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link href="/employees">Employees</Nav.Link>
-                        <Nav.Link href="/pricing">Pricing</Nav.Link>
+                        {navlinks.map((link)=>{
+                            return (
+                            <Nav.Link 
+                                as={NavLink} 
+                                to={link.Path} 
+                                key={link.Path} 
+                                end={link.Path === '/'}
+                                style={({isActive})=>({
+                                    fontWeight: isActive ? 'bold' : 'normal',
+                                    color: isActive ? 'white' : 'rgba(255, 255, 255, 0.55)'
+                                })}>
+                                    {link.Name}
+                            </Nav.Link>)
+                        })}
                     </Nav>
                 </Container>
             </Navbar>
